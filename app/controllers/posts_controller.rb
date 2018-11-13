d2da3be0018761c1f1cd87b217451069cd186cd0
Oskar <sobczykoskar@gmail.com>
2018-11-13 20:37:00 +0100

    Adjust delay for worker

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -57,7 +57,7 @@ class PostsController < ApplicationController
     post_id = params[:id]
     author_id = current_author.id
 
-    NotificationWorker.perform_in(1.second, author_id: author_id, post_id: post_id)
+    NotificationWorker.perform_in(5.minute, author_id: author_id, post_id: post_id)
 
     redirect_back fallback_location: '/'
   end
