a623a39be56ffd0ba8e36fb850e40132ce79bb5d
Marek <marekkwasny96@gmail.com>
2018-12-08 00:10:26 +0100

    Add proper notice after vote removal

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -9,15 +9,15 @@ class VotesController < ApplicationController
     @vote = Vote.find_or_initialize_by(author_id: current_author.id, voting_object_id: params[:voting_object_id], voting_object_type: params[:voting_object_type])
     if @vote.vote_type == params[:vote_type].to_i
       Vote.destroy(@vote.id)
+      flash[:notice] = 'Removed' if @vote.save
     else
       @vote.vote_type = params[:vote_type]
-    end
-
-    if @vote.save
-      flash[:notice] = 'Upvoted' if @vote.vote_type.zero?
-      flash[:notice] = 'Downvoted' if @vote.vote_type == 1
-    else
-      flash[:alert] = @vote.errors.full_messages.join('. ')
+      if @vote.save
+        flash[:notice] = 'Upvoted' if @vote.vote_type.zero?
+        flash[:notice] = 'Downvoted' if @vote.vote_type == 1
+      else
+        flash[:alert] = @vote.errors.full_messages.join('. ')
+      end
     end
     redirect_back fallback_location: '/'
   end
