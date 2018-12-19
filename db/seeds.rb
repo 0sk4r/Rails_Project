61e7e9075de272ba16a4609b6f5d132c75fed15b
Oskar <sobczykoskar@gmail.com>
2018-12-19 12:14:45 +0100

    Add testing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-Author.create!([
-                 { nick: 'a', email: 'a@a.pl', password: '123456', reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil }
-               ])
 Category.create!([
                    { name: 'all' }
                  ])
-Post.create!([
-               { title: 'sdf', content: 'fasfdasf', author_id: 1, category_id: 1 }
-             ])
+
+(1..100).each do |id|
+  Author.create!([
+                   { nick: "author#{id}", email: "author#{id}@author.pl", password: '123456' }
+                 ])
+  Post.create!([
+                 { title: Faker::Hacker.abbreviation,
+                   content: Faker::Hacker.say_something_smart,
+                   author_id: id,
+                   category_id: 1 }
+               ])
+end
